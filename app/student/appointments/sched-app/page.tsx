d65a9bc1fa459fdbdbbf545f99542ef8bf49da62
Mark Rensel Yeban <mdyeban@up.edu.ph>
2025-05-07 05:03:17 +0800

    little testing

--- app/student/appointments/sched-app/page.tsx
+++ app/student/appointments/sched-app/page.tsx
@@ -578,13 +578,16 @@ export function AddAppointmentForm() {
                       </FormControl>
                     </PopoverTrigger>
                     <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        disabled={(date) => date < new Date()}
-                        initialFocus
-                      />
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={(value) => {
+                      field.onChange(value);
+                      setSelectedDate(value);
+                      }}
+                      disabled={(date) => date < new Date()}
+                      initialFocus
+                  />
                     </PopoverContent>
                   </Popover>
                   <FormMessage className="text-xs" />
